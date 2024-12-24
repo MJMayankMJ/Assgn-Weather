a3dfb8714d07b681879389a4d428ae18c9adf606
MJMayankMJ <226301131@gkv.ac.in>
2024-12-24 16:16:26 +0530

    Initial Commit

---
+++ Assgn Weather/Assgn_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assgn_WeatherApp.swift
+//  Assgn Weather
+//
+//  Created by Mayank Jangid on 12/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Assgn_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assgn WeatherTests/Assgn_WeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  Assgn_WeatherTests.swift
+//  Assgn WeatherTests
+//
+//  Created by Mayank Jangid on 12/24/24.
+//
+
+import Testing
+@testable import Assgn_Weather
+
+struct Assgn_WeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
